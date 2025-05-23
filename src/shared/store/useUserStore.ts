0afb4414dc2bb052d8a81a790a47bf6dae90bdea
HoeunRyu <hoeun9565@gmail.com>
2025-05-23 14:38:10 +0900

    add: 유저스토어 주석 추가

--- src/shared/store/useUserStore.ts
+++ src/shared/store/useUserStore.ts
@@ -2,6 +2,21 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { UserType } from '../types/types';
 
+// TODO: 로그인 api 완성되면 데이터타입 맞는지 확인해보기
+
+// 예시: 일반유저 로그인 성공 시 / 프로필 등록 완료 시 response data로 아래 2가지 작업을 하면 됩니다.
+// 1. setUserInfo(userType: 'customer', userInfo: {
+//     id: data.id,
+//     username: data.username,
+//     email: data.email,
+//     phoneNumber: data.phoneNumber,
+//     profileImage: data.profileImage,
+//   });
+// 2. setCustomerData(customerData: {
+//     wantService: data.wantService,
+//     livingPlace: data.livingPlace,
+//   });
+
 // 최초 로그인시 일반 유저 / 기사님 공통으로 가지는 데이터
 // - 헤더 프로필에 usernamd, profileImage 사용
 export interface UserInfo {
