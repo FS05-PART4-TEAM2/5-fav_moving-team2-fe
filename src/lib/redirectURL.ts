5c878f9eac98ec4f875cd695cd1a05907fcf01f2
HEES56 <rlagmltjd125@gmail.com>
2025-05-27 17:13:29 +0900

    [fix]: 회원가입도 리다이렉트 저장 주소 제외 처리

--- src/lib/redirectURL.ts
+++ src/lib/redirectURL.ts
@@ -3,7 +3,9 @@ import { PATH } from '@/shared/constants';
 export function redirectURL(userType: 'customer' | 'mover'): string {
   const currentPath = window.location.pathname + window.location.search;
 
-  if (!currentPath.includes('/login')) {
+  const ExcludePath = currentPath.includes('/login') || currentPath.includes('/signup') || currentPath === '/';
+
+  if (!ExcludePath) {
     localStorage.setItem('redirectAfterLogin', currentPath);
   }
 
