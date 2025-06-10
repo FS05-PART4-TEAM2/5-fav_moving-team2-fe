13de353ee36632ffd43b770f728382d1b4efa680
HoeunRyu <hoeun9565@gmail.com>
2025-06-10 09:50:42 +0900

    fix: 필요없는 코드 삭제

--- src/shared/utils/invalidateQueryKeys.tsx
+++ src/shared/utils/invalidateQueryKeys.tsx
@@ -1,6 +1,5 @@
 import { QueryClient } from '@tanstack/react-query';
 import { moverKeys } from './queryKeys';
-import { useSearchMoverStore } from '@/app/customer/search-mover/core/hooks/useSearchMoverStore';
 
 // 이후 쿼리키 추가되면 여기에도 같이 추가
 export const invalidateQueryKeys = (queryClient: QueryClient) => {
