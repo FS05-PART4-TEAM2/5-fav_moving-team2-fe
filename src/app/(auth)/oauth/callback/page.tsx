fb6295ac15eed3580f7e6948f423dc04f7d82dd9
hyuk-dev <hyuk.development@gmail.com>
2025-06-12 18:54:34 +0900

    fix: oauth 폴더로 새로 생성 된 page.tsx 수정

--- src/app/(auth)/oauth/callback/page.tsx
+++ src/app/(auth)/oauth/callback/page.tsx
@@ -44,7 +44,7 @@ export default function OAuthCallbackPage() {
         const res = await OAuthProfile(type);
 
         if (type === 'customer') {
-          const user = res.data as GetCustomerProfileData;
+          const user = res.data as GetCustomerProfileData
           setUserInfo('customer', {
             id: user.id,
             username: user.username,
