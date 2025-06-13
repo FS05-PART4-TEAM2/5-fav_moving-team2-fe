3e7710ca8f141a139ed0cec3382ace9d91007f38
HEES56 <rlagmltjd125@gmail.com>
2025-06-13 15:06:18 +0900

    [ADD]: mover-mypage

---
+++ src/app/mover/my-page/_core/components/ReviewSummary.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { Box, Stack, useMediaQuery } from '@mui/material';
+import { Typo } from '@/shared/styles/Typo/Typo';
+import theme from '@/shared/theme';
+import { colorChips } from '@/shared/styles/colorChips';
+import ProgressBar from '@/shared/components/ProgressBar/ProgressBar';
+import Image from 'next/image';
+
+const scores = [
+  { score: 5, count: 170 },
+  { score: 4, count: 8 },
+  { score: 3, count: 0 },
+  { score: 2, count: 0 },
+  { score: 1, count: 0 },
+];
+
+export default function ReviewSummary() {
+  const total = scores.reduce((acc, cur) => acc + cur.count, 0);
+
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  return (
+    <Stack
+      direction={isDesktop ? 'row' : isMobile ? 'column' : 'row'}
+      alignItems="center"
+      justifyContent="center"
+      bgcolor={isDesktop ? colorChips.background.f7f7f7 : ''}
+      borderRadius="32px"
+      py={isDesktop ? '40px' : ''}
+      mt="32px"
+      gap={isDesktop ? '83px' : isMobile ? '40px' : '53px'}
+    >
+      {/* 왼쪽 별점 평균 */}
+      <Stack direction="column" alignItems="center">
+        <Stack direction="row" gap="8px" alignItems="center">
+          <Typo className="text_B_40to64" style={{ color: colorChips.black[300] }} content="5.0" />
+          <Typo className="text_B_24to38" content="/ 5" style={{ color: colorChips.grayScale[100] }} />
+        </Stack>
+        <Stack direction="row" spacing="2px" mt="4px">
+          {Array.from({ length: 5 }).map((_, idx) => (
+            <Image
+              key={idx}
+              src="/assets/images/star-icon/star-yellow-24x24.svg"
+              alt="star icon"
+              width={isDesktop ? 48 : 24}
+              height={isDesktop ? 48 : 24}
+            />
+          ))}
+        </Stack>
+      </Stack>
+
+      {/* 오른쪽 점수 분포 */}
+      <Stack
+        bgcolor={colorChips.background.f7f7f7}
+        borderRadius="32px"
+        py={isDesktop ? '' : '16px'}
+        px={isDesktop ? '' : '21.5px'}
+      >
+        {scores.map(({ score, count }) => (
+          <Stack key={score} direction="row" alignItems="center" gap={isDesktop ? '30px' : '16px'}>
+            <Typo
+              className="text_B_14to20"
+              style={{ color: colorChips.black[300], textWrap: 'nowrap' }}
+              content={`${score}점`}
+            />
+            <Box width={isDesktop ? '370px' : '180px'}>
+              <ProgressBar type="review" percentage={total ? (count / total) * 100 : 0} />
+            </Box>
+            <Typo
+              className="text_B_14to20"
+              style={{ color: colorChips.grayScale[300], textWrap: 'nowrap' }}
+              content={`${count}`}
+            />
+          </Stack>
+        ))}
+      </Stack>
+    </Stack>
+  );
+}
--- src/app/mover/my-page/page.tsx
+++ src/app/mover/my-page/page.tsx
@@ -2,12 +2,57 @@
 
 import { colorChips } from '@/shared/styles/colorChips';
 import { Typo } from '@/shared/styles/Typo/Typo';
-import { Stack } from '@mui/material';
+import theme from '@/shared/theme';
+import { Divider, Stack, useMediaQuery } from '@mui/material';
+import { mockQuotation } from '../moving-quote/history/core/mock';
+import Card from '@/shared/components/Card/Card';
+import ReviewSummary from './_core/components/ReviewSummary';
+import { CommonPagination } from '@/shared/components/Pagination/CommonPagination';
+import { useEffect, useState } from 'react';
 
 export default function Page() {
+  const [page, setPage] = useState(1);
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+  const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+  const itemsPerPage = isDesktop ? 5 : isTablet ? 4 : 3;
+
+  const totalReviews = mockQuotation.length;
+  const startIdx = (page - 1) * itemsPerPage;
+  const pagedReviews = mockQuotation.slice(startIdx, startIdx + itemsPerPage);
+  const totalPages = Math.ceil(totalReviews / itemsPerPage);
+
+  useEffect(() => {
+    if (page > totalPages) {
+      setPage(totalPages);
+    }
+  }, [itemsPerPage, totalPages]);
+
+  //TODO  ReviewSummary :총 리뷰 별점 토탈,1~5 점수 각각 토탈 count 넘겨주기
+  //      리뷰 리스트 : 데이터 받아와서 맵핑
   return (
-    <Stack height="100%" bgcolor={colorChips.secondary.yellow[100]}>
-      <Typo className="text_B_20" content="기사님 마이페이지" />
+    <Stack height="100%">
+      <Stack py={isDesktop ? '32px' : '15px'}>
+        <Typo className="text_SB_14to24" style={{ color: colorChips.black.b2b2b }} content="마이페이지" />
+      </Stack>
+
+      <Stack pt="24px" gap={isDesktop ? '40px' : '24px'}>
+        <Card type="profile" data={mockQuotation[0].data} bgColor />
+
+        <Divider sx={{ borderColor: colorChips.line['f2f2f2'] }} />
+        <Stack>
+          <Typo className="text_B_16to24" style={{ color: colorChips.black.b2b2b }} content="리뷰 (178)" />
+          <ReviewSummary />
+        </Stack>
+
+        <Stack>
+          {pagedReviews.map((review, idx) => (
+            <Card key={idx} type="review" data={review.data} />
+          ))}
+          <Stack alignItems="center">
+            <CommonPagination page={page} totalCount={totalPages} handleChange={(event, value) => setPage(value)} />
+          </Stack>
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
